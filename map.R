f626ac149d624287d1901b41c7de36b4235542f5
unknown <lijiaping@sz4995.genomics.cn>
2015-09-01 16:40:30 +0800

    using RISmed to get PubMed Address, using Google map API to get lat and lng

--- map.R
+++ map.R
@@ -1,5 +1,7 @@
 library(leaflet)
+library(XML)
 library(RISmed)
+library(ggmap)
 
 load('omictools.RData')
 geocode<-function(address){
@@ -19,55 +21,80 @@ address_df<-software_df %>%
          pid = gsub('.*/', '', PubMed) ) %>%
   select(id, pid, Created_at)
 
-
 pids<-address_df %>%
   filter(!is.na(pid),
          grepl('^[0-9]+$', pid),
-         nchar(pid) >= 6 ) %>%
+         nchar(pid) >= 7 ) %>%
   "$"('pid') %>%
   unique
 
-results<-EUtilsGet(pids[1:10])
-pubmed_address<-data.frame(pid = PMID(results),
-                           affiliation = Affiliation(results),
-                           stringsAsFactors = F)
+fetch<-EUtilsGet(pids)
 
-addresses<-gsub('\\..*$','',pubmed_address$affiliation)
+# length(pids)
+# 
+# pids<-lapply(0:17, function(x){
+#   start<-x*500 + 1
+#   end<-(x+1)*500
+#   pid<-pids[start:end]
+#   idx<-!is.na(pid)
+#   pid[idx]
+# })
+# 
+# fetch<-llply(pids, function(pid){
+#   EUtilsGet(pid)
+#   Sys.sleep(10)
+#   }, .progress='text')
+
+#save(fetch, address_pubmed_df, file='PubMed.RData')
+load('PubMed.RData')
+
+address_pubmed_df<-data.frame(pid = PMID(fetch),
+                           affiliation = Affiliation(fetch),
+                           cited = Cited(fetch),
+                           stringsAsFactors = F) %>%
+  merge(address_df, by='pid', all.y=T) %>%
+  mutate(na.idx = is.na(affiliation),
+         Created_at = as.character(Created_at)) %>%
+  within(affiliation[na.idx] <- Created_at[na.idx]) %>%
+  select(id, pid, affiliation, cited)
+
+na.idx<-is.na(address_pubmed_df$affiliation)
+addresses<-gsub('\\..*$','',address_pubmed_df$affiliation[!na.idx])
+
+address_lat_lng<-geocode(addresses, output='more')
+
+getGeo<-function(addresses){
+  llply(addresses, function(addr){
+    geocode(URLencode(addr))
+    Sys.sleep(0.2)
+  },.progress='text')
+}
+
+
+addresses<-lapply(0:17, function(x){
+  start<-x*500 + 1
+  end<-(x+1)*500
+  addresses<-addresses[start:end]
+  idx<-!is.na(addresses)
+  addresses[idx]
+})
 
 addresses_lat_lng<-llply(addresses, function(addr){
-  geocode(URLencode(addr))
-},.progress='text')
+  Sys.sleep(20)
+  getGeo(addr)
+  },.progress='text')
 
-names(addresses_lat_lng)<-pubmed_address$pid
 
-addr<-ldply(lapply(addresses_lat_lng, as.data.frame), .id='pid') %>%
-  filter(!is.na(lat))
+names(addresses_lat_lng)<-address_pubmed_df$id[!na.idx]
 
-leaflet(addr) %>%
+addr_lat_lng<-ldply(lapply(addresses_lat_lng, as.data.frame), .id='id') %>%
+  filter(!is.na(lat)) %>%
+  merge(address_pubmed_df, by='id')
+
+leaflet(addr_lat_lng) %>%
   setView(lng=124, lat=26, zoom=1) %>%
   addTiles() %>%
-  addCircleMarkers(~lng, ~lat, popup = ~pid,
-                   stroke = F)
-
-
-
-addresses2<-address_df %>%
-  filter(is.na(pid),
-         !is.na(Created_at))
-
-addresses<-gsub('\\..*$','',addresses2$Created_at)
-
-addresses_lat_lng2<-llply(addresses, function(addr){
-  geocode(URLencode(addr))
-},.progress='text')
-
-names(addresses_lat_lng2)<-addresses2$id
-
-addr2<-ldply(lapply(addresses_lat_lng2, as.data.frame), .id='id') %>%
-  filter(!is.na(lat))
-
-leaflet(addr2) %>%
-  setView(lng=124, lat=26, zoom=1) %>%
-  addTiles() %>%
-  addCircleMarkers(~lng, ~lat, popup = ~id,
+  addCircleMarkers(~lng, ~lat,
+                   radius = ~sqrt(cited),
+                   popup = ~id,
                    stroke = F)
