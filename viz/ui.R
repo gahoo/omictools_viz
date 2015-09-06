c9b4648d0d312ae7df4714f4c291051f3bb374a1
unknown <lijiaping@sz4995.genomics.cn>
2015-09-06 17:17:08 +0800

    change layout, add leaflet

--- viz/ui.R
+++ viz/ui.R
@@ -1,15 +1,18 @@
 library(shiny)
 library(d3treeR)
 library(DT)
+library(leaflet)
 
 
 shinyUI(fluidPage(
   titlePanel('Omictools.com Visualization'),
   fluidRow(
-    column(6, d3tree2Output( 'tree' )),
+    column(6,
+           d3tree2Output('tree'),
+           leafletOutput('map') ),
     column(6, 
-           strong(textOutput( "clickedinfo" )),
+           strong(textOutput("clickedinfo")),
            DT::dataTableOutput('catalog'),
-           DT::dataTableOutput('detail'))
+           DT::dataTableOutput('detail', width="100%"))
   )
 ))
