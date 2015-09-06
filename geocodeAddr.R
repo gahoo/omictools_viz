eaa2ebf27e2cdcccfb78307479393e042b9de80d
unknown <lijiaping@sz4995.genomics.cn>
2015-09-06 17:11:28 +0800

    encapsulate major part as a function

--- geocodeAddr.R
+++ geocodeAddr.R
@@ -20,13 +20,37 @@ addresses<-address_gather_pubmed_df$address
 address_lat_lng<-list()
 load('address_lat_lng.RData')
 start<-length(address_lat_lng)+1
-llply(start:nrow(address_gather_pubmed_df), function(i){
-  address_lat_lng[[i]]<<-geocode(addresses[i], source = 'google',
-                                 output='all', messaging = F)
-  if(geocodeQueryCheck()<200){
-    break
-  }
-  Sys.sleep(0.2)
-}, .progress = 'text')
+rest_idx<-start:nrow(address_gather_pubmed_df)
 
-save(address_lat_lng, file='address_lat_lng.RData')
+geocodeIdx<-function(idx){
+  llply(idx, function(i){
+    address_lat_lng[[i]]<<-geocode(addresses[i], source = 'google',
+                                   output='all', messaging = F)
+    #if(address_lat_lng[[i]]$status == 'OVER_QUERY_LIMIT'){
+    if(geocodeQueryCheck()<200){
+      break
+    }
+    Sys.sleep(0.2)
+  }, .progress = 'text')
+}
+
+#geocodeIdx(rest_idx)
+#save(address_lat_lng, file='address_lat_lng.RData')
+
+status<-sapply(address_lat_lng, function(x){unlist(x['status'])})
+retry_idx<-which(status == 'OVER_QUERY_LIMIT' |is.na(status))
+zero_idx<-which(status == 'ZERO_RESULTS')
+
+geocodeIdx(retry_idx)
+
+
+load('omictools_tree.RData')
+
+cid2sid<-function(cid){
+  child.ids<-subset(catalog_tree_df, parent.id == cid)$id
+  if(length(child.ids) == 0){
+    subset(software_tree_df, parent.id == cid)$id
+  }else{
+    unique(unlist(lapply(child.ids, cid2sid)))
+  }
+}
